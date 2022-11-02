44aa72e29e4ba66a9bd788acef1ae61ef30b94cf
CSE19-064 <cse19-064@thuto.bac.ac.bw>
2022-11-02 17:38:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/babybuy/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.babybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+/*  UI LINKS
+* https://www.youtube.com/watch?v=je2T3sRBPjk&list=PLQ3Uv2wGPHIJzEdjTVu4Yicrj3rR4DPKc
+* https://youtu.be/q4ad5ozI8y0
+*https://youtu.be/je2T3sRBPjk
+* */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

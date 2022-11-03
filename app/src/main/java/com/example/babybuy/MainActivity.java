47a7cf5056cca3220365d231777a20fb582a59fc
cse19-064 <cse19-064@thuto.bac.ac.bw>
2022-11-03 12:00:24 +0200

    splash

--- app/src/main/java/com/example/babybuy/MainActivity.java
+++ app/src/main/java/com/example/babybuy/MainActivity.java
@@ -8,11 +8,12 @@ public class MainActivity extends AppCompatActivity {
 /*  UI LINKS
 * https://www.youtube.com/watch?v=je2T3sRBPjk&list=PLQ3Uv2wGPHIJzEdjTVu4Yicrj3rR4DPKc
 * https://youtu.be/q4ad5ozI8y0
-*https://youtu.be/je2T3sRBPjk
+* https://youtu.be/je2T3sRBPjk
 * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
